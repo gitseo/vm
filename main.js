83da0107a4b0550a08b57554deb755bce4003b47
adler3d <adler3d@gmail.com>
2020-10-06 15:41:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -218,9 +218,8 @@ var toHHHMMSS=s=>[s/3600,s/60,s].map(x=>x|0).map((x,i)=>i?x%60:x).map(v=>v<10?"0
 
 var func_to_var_decl=func=>"var "+func+"="+eval("("+func+").toString()")+";\n";
 
-var parse_datetime2=s=>{
-  var t=s.split(' ');
-  if(t[0].includes(":")&&t.length==2){t.reverse();}
+var parse_datetime=s=>{
+  var t=s.split(' ');if(t[0].includes(":")&&t.length==2){t.reverse();}
   var ymd=t[0].split('.');var hms=t[1].split(':');
   if(hms[2].includes(".")){
     let a=hms[2].split(".");
