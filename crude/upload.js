2826663c8205b1615bb4645edadb317b69dc61f5
adler3d <adler3d@gmail.com>
2020-10-06 14:28:30 +0300

    Update upload.js

--- crude/upload.js
+++ crude/upload.js
@@ -1,5 +1,5 @@
 var multiparty=hack_require('multiparty');
-return html('<center><form action="/upload" enctype="multipart/form-data" method="post">'+
+return html('<center style="font-size:300%;"><form action="/upload" enctype="multipart/form-data" method="post">'+
     '<input type="text" name="title"><br>'+
     '<input type="file" name="upload" multiple="multiple"><br>'+
     '<input type="submit" value="Upload">'+
